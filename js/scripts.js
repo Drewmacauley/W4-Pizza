7ce66e7c2d61454b77d2d200947626f79256d9e7
Drewmacauley <macauleyandy03@gmail.com>
2020-08-28 11:41:33 -0700

    add start of UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 //Business Logic for the user Object
-function addPizza (topping, size, cost) {
+function AddPizza (topping, size, cost) {
 this.topping = topping,
 this.size = size,
 this.cost = cost;
 }
 
-let addPizza = (0,0,0);
+let toppings = new AddPizza (0,0,0);
+let size = new AddPizza (0,0,0);
 
-addPizza.prototype.topping = function(cheese, pep, salami, pineapple, Ham, peppers) {
+AddPizza.prototype.topping = function(cheese, pep, salami, pineapple, Ham, peppers) {
 let topping = 1
 }
 
