8d6798ce282ea69574a504dec7eefcfa0c2562d6
Drewmacauley <macauleyandy03@gmail.com>
2020-08-28 12:07:54 -0700

    add new thought to UI

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,22 @@ let toppings = new AddPizza (0,0,0);
 let size = new AddPizza (0,0,0);
 
 AddPizza.prototype.topping = function(cheese, pep, salami, pineapple, Ham, peppers) {
-let topping = 1
+if (topping === 1) {
+  this.cost += 1
+}
 }
 
+
+
+
+
+
+
+
+//User Interface Logic
+$(document).ready(function(){
+
+  $("#totalCost").click(function() {
+    let topping = size.cost() //we have topping = cost????? cost not total cost.....should they be =? should we have cost there?
+  })
+})
